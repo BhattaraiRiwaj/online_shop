3f111bd601472a3f96d4786086aaf579a9325dac
Riwaj Bhattarai <rbz.bhattarai@gmail.com>
2024-03-16 15:10:47 +0545

    changes in web

--- routes/web.php
+++ routes/web.php
@@ -25,62 +25,59 @@ Route::get('/', function () {
 });
 
 
-Route::group(['prefix'=>'admin'],function(){
+Route::group(['prefix' => 'admin'], function () {
 
-    Route::group(['middleware'=>'admin.guest'],function(){
-        Route::get('/login',[AdminLoginController::class,'index'])->name('admin.login');
-        Route::post('/auhenticate',[AdminLoginController::class,'auhenticate'])->name('admin.auhenticate');
+    Route::group(['middleware' => 'admin.guest'], function () {
+        Route::get('/login', [AdminLoginController::class, 'index'])->name('admin.login');
+        Route::post('/auhenticate', [AdminLoginController::class, 'auhenticate'])->name('admin.auhenticate');
     });
 
 
 
-    Route::group(['middleware'=>'admin.auth'],function(){
-        Route::get('/dashboard',[HomeController::class,'index'])->name('admin.dashboard');
-        Route::get('/logout',[HomeController::class,'logout'])->name('admin.logout');
+    Route::group(['middleware' => 'admin.auth'], function () {
+        Route::get('/dashboard', [HomeController::class, 'index'])->name('admin.dashboard');
+        Route::get('/logout', [HomeController::class, 'logout'])->name('admin.logout');
 
 
         //category Routes
-        Route::prefix('category/')->name('categories.')->group(function(){
-            Route::get('index',[CategoryController::class,'index'])->name('index');
-            Route::get('create',[CategoryController::class,'create'])->name('create');
-            Route::post('store',[CategoryController::class,'store'])->name('store');
-            Route::get('{id}/edit',[CategoryController::class,'edit'])->name('edit');
-            Route::patch('{id}',[CategoryController::class,'update'])->name('update');
+        Route::prefix('category/')->name('categories.')->group(function () {
+            Route::get('index', [CategoryController::class, 'index'])->name('index');
+            Route::get('create', [CategoryController::class, 'create'])->name('create');
+            Route::post('store', [CategoryController::class, 'store'])->name('store');
+            Route::get('{id}/edit', [CategoryController::class, 'edit'])->name('edit');
+            Route::patch('{id}', [CategoryController::class, 'update'])->name('update');
 
-            Route::delete('{id}',[CategoryController::class,'destroy'])->name('delete');
+            Route::delete('{id}', [CategoryController::class, 'destroy'])->name('delete');
 
             //status
-            Route::get('status/{id}',[CategoryController::class,'userStatus'])->name('status');
+            Route::get('status/{id}', [CategoryController::class, 'userStatus'])->name('status');
 
             // temp-images.create
-            Route::post('temp-image/upload',[TempImagesController::class,'create'])->name('temp-images.create');
+            Route::post('temp-image/upload', [TempImagesController::class, 'create'])->name('temp-images.create');
 
             //getslug
-            Route::get('getSlug',function (Request $request){
+            Route::get('getSlug', function (Request $request) {
                 $slug = '';
-                if(!empty($request->title)){
+                if (!empty($request->title)) {
                     $slug = Str::slug($request->title);
                     return response()->json([
-                    'status'=>true,
-                    'slug'=>$slug,
+                        'status' => true,
+                        'slug' => $slug,
                     ]);
                 }
             })->name('slug');
         });
 
-         //sub category Routes
-         Route::name('sub_categories.')->group(function(){
-            Route::get('sub_category/index',[SubCategoryController::class,'index'])->name('index');
-            Route::get('sub_category/create',[SubCategoryController::class,'create'])->name('create');
-            Route::post('sub_category/store',[SubCategoryController::class,'store'])->name('store');
-            Route::get('sub_category/{id}/edit',[SubCategoryController::class,'edit'])->name('edit');
-            Route::patch('sub_category/{id}',[SubCategoryController::class,'update'])->name('update');
-            Route::delete('sub_category/{id}',[SubCategoryController::class,'destroy'])->name('delete');
-        //status
-         Route::get('status/{id}',[SubCategoryController::class,'userStatus'])->name('status');
-
-         });
-
-
+        //sub category Routes
+        Route::name('sub_categories.')->group(function () {
+            Route::get('sub_category/index', [SubCategoryController::class, 'index'])->name('index');
+            Route::get('sub_category/create', [SubCategoryController::class, 'create'])->name('create');
+            Route::post('sub_category/store', [SubCategoryController::class, 'store'])->name('store');
+            Route::get('sub_category/{id}/edit', [SubCategoryController::class, 'edit'])->name('edit');
+            Route::patch('sub_category/{id}', [SubCategoryController::class, 'update'])->name('update');
+            Route::delete('sub_category/{id}', [SubCategoryController::class, 'destroy'])->name('delete');
+            //status
+            Route::get('status/{id}', [SubCategoryController::class, 'userStatus'])->name('status');
+        });
     });
 });
